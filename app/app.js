d9e05da86ae3261879ee108bdc2e32558d6c7921
Isamu Endo <alupaca1363@gmail.com>
2025-03-09 00:21:55 +0900

    message

--- app/app.js
+++ app/app.js
@@ -9,7 +9,7 @@ const server = http.createServer((req, res) => {
 
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!!!!');
+  res.end('Hello pino!!!!');
 });
  
 server.listen(port, hostname, () => {
